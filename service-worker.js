cdca4ef8cf13cdb556cd223d483f1836a1d5c8d4
YoelEigner <35924447+YoelEigner@users.noreply.github.com>
2024-04-03 18:12:16 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f93ad6b8d04637d72bb03cf010cf916a.js"
+  "/precache-manifest.4c15d389e1fc04f29b2fc298bc4970f6.js"
 );
 
 self.addEventListener('message', (event) => {
